6770e19c6f79c0a963d4b3378d0e33e6aa645274
wendersonads <wendersonads5.5@gmail.com>
2023-09-28 21:36:14 -0300

    alteracoes

---
+++ lib/domain/service/login_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:auth_migration/core/auth/token_service.dart';
+import 'package:auth_migration/domain/model/dto/login_dto.dart';
+import 'package:auth_migration/domain/model/token_model.dart';
+import 'package:auth_migration/base/service/base_service.dart';
+import 'package:auth_migration/base/custom/custom_http.dart';
+import 'package:http/http.dart';
+
+class LoginService {
+  TokenService tokenService = TokenService();
+
+  final BaseService _abstractService = BaseService('');
+
+  Future<bool> tryLogin(String username, String password) async {
+    LoginDTO login = LoginDTO(username: username, password: password);
+
+    print(login.toJson());
+
+    Response response = await CustomHttp.post(
+        await _abstractService.getUrl('login'),
+        headers: login.toMap(),
+        body: login.toJson(),
+        secure: false);
+    tokenService.save(response.body);
+    return true;
+  }
+}
